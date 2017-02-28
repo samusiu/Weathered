86e8672523e5e12632917134e6f187b37a679ada
Janine Jay <janinerjay@aol.com>
2017-02-28 13:27:33 +0000

    Merge branch 'map-with-pinned-locations'
    # Conflicts:
    #	placesAPI.html
    #	src/components/iphone/index.js
    #	src/components/map/index.js
    #	src/components/map/style_ipad.less
    #	src/components/map/style_iphone.less

--- src/components/iphone/index.js
+++ src/components/iphone/index.js
@@ -8,6 +8,7 @@ import style_iphone from '../button/style_iphone';
 import $ from 'jquery';
 // import the Button component
 import Button from '../button';
+import Map from '../map';
 
 export default class Iphone extends Component {
 //var Iphone = React.createClass({
@@ -19,6 +20,60 @@ export default class Iphone extends Component {
 		this.state.temp = "";
 		// button display state
 		this.setState({ display: true });
+		var map;
+	}
+
+<<<<<<< HEAD
+	initialize() {
+		var center = new google.maps.LatLng(51.526806,-0.0419017);
+		this.map = new google.maps.Map(document.getElementById('map'), {
+			center: center,
+			zoom: 13
+		});
+
+		google.maps.event.addDomListener(window, 'load', this.initialize);
+		var request1 = {
+			location: center,
+			radius: 8000,
+			types: ['restaurant']
+		};
+		var request2 = {
+			location: center,
+			radius: 8000,
+			types: ['park']
+		};
+
+		var service = new google.maps.places.PlacesService(map);
+
+		service.nearbySearch(request1, this.callback);
+		service.nearbySearch(request2, this.callback);
+	}
+
+	callback(result, status) {
+		if(status == google.maps.places.PlacesServiceStatus.OK){
+			for(var i = 0; i < result.length; i++){
+				createMarker(result[i]);
+=======
+	initialize(parsed_json) {
+		if(parsed_json.status == google.maps.places.PlacesServiceStatus.OK){
+			var center = new google.maps.LatLng(51.526806,-0.0419017);
+			this.map = new google.maps.Map(document.getElementById('map'), {
+				center: center,
+				zoom: 13
+			});
+			var place;
+			for(var i = 0; i < parsed_json.results.length; i++){
+				place = parsed_json.results[i];
+				var placeLoc = place.geometry.location;
+				var marker = new google.maps.Marker({
+					map: this.map,
+					position: place.geometry.location
+				});
+>>>>>>> map-with-pinned-locations
+			}
+		}else {
+			console.log("Invalid json");
+		}
 	}
 
 	// a call to fetch weather data via wunderground
@@ -34,7 +89,28 @@ export default class Iphone extends Component {
 		// once the data grabbed, hide the button
 		this.setState({ display: false });
 	}
+<<<<<<< HEAD
+=======
+	getPlaces = () => {
+		//API info: https://developers.google.com/places/web-service/search
+		var location = '51.5238447,-0.0404668';
+		var radius = '5000';
+		var type = 'park';
+		//supported types here: https://developers.google.com/places/web-service/supported_types
+		var api_url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+location+'&radius='+radius+'&type='+type+'&key=AIzaSyBiXC1s3oFkNEejJIRcMIB2E3AcUUEacH4';
+		$.ajax({
+		 url: api_url,
+		 dataType: 'json',
+		 success : this.initialize,
+		 error : function(req, err){ console.log('API call failed ' + err); }
+ });
+	}
+>>>>>>> map-with-pinned-locations
 
+	createMap(parsed_json){
+		initialize(parsed_json);
+		google.maps.event.addDomListener(window, 'load', initialize);
+	}
 	// the main render method for the iphone component
 	render() {
 		// check if temperature data is fetched, if so add the sign styling to the page
@@ -43,18 +119,24 @@ export default class Iphone extends Component {
 		// display all weather data
 		return (
 			<div class={ style.container }>
+<<<<<<< HEAD
+			<Map class={style_iphone.map}/>
+=======
 				<div class={ style.header }>
 					<div class={ style.city }>{ this.state.locate }</div>
 					<div class={ style.conditions }>{ this.state.cond }</div>
 					<span class={ tempStyles }>{ this.state.temp }</span>
 				</div>
+				<Map style="height: 100%"/>
 				<div class={ style.details }></div>
+>>>>>>> map-with-pinned-locations
 				<div class= { style_iphone.container }>
-					{ this.state.display ? <Button class={ style_iphone.button } clickFunction={ this.fetchWeatherData }/ > : null }
+					{ this.state.display ? <Button class={ style_iphone.button } clickFunction={ this.initialize }/ > : null }
 				</div>
 			</div>
 		);
 	}
+<<<<<<< HEAD
 
 	parseResponse = (parsed_json) => {
         //Variables to get form URL
@@ -69,4 +151,6 @@ export default class Iphone extends Component {
 			cond : conditions
 		});
 	}
+=======
+>>>>>>> map-with-pinned-locations
 }
---
+++ src/components/map/index.js
@@ -0,0 +1,17 @@
+// import preact
+import { h, render, Component } from 'preact';
+
+export default class Map extends Component {
+
+	// rendering a function when the button is clicked
+	render() {
+		return (
+<<<<<<< HEAD
+			<div id="map" style="height:200%">
+=======
+			<div id="map">
+>>>>>>> map-with-pinned-locations
+			</div>
+		);
+	}
+}
