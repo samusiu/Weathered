41d27dfae60821812c9976ea86622f1a4666e965
Hannah <hholman1902@gmail.com>
2017-03-08 17:47:27 +0000

    adding weekly component

---
+++ src/components/testButton/index.js
@@ -0,0 +1,22 @@
+// import preact
+import { h, render, Component } from 'preact';
+import style from './style';
+
+export default class TestButton extends Component {
+
+	// rendering a function when the button is clicked
+	render({clickFunction}) {
+		if(!clickFunction){
+			clickFunction = () => {
+				console.log("passed something as 'clickFunction' that wasn't a function !");
+			}
+		}
+		return (
+			<div>
+				<button class={style.button} onClick={clickFunction}>
+                    <img class = {style.iconarrow} src="../../assets/images/Arrow.png"/>
+				</button>
+			</div>
+		);
+	}
+}
---
+++ src/components/weekly/index.js
@@ -0,0 +1,75 @@
+// import preact
+import { h, render, Component } from 'preact';
+// import stylesheets for ipad & button
+import style from './style';
+import style_iphone from '../button/style_iphone';
+// import jquery for API calls
+import $ from 'jquery';
+// import the Button component
+import Button from '../button';
+import Weather from '../weather';
+import Map from '../map';
+import Iphone from '../iphone';
+
+export default class Weekly extends Component {
+
+	/** a constructor with initial set states
+	 * @param props the properties of the component
+	 */
+	constructor(props){
+		super(props);
+		this.setState({ display : true });
+	}
+
+	/** Retrieves the weekly weather conditions. Passed in as props. 
+	 * @return weeklyWeather the weekly weather conditions
+	 */
+	weeklyWeather = () => {
+		return this.props.weeklyWeather;
+	}
+
+	/** Toggles display of weekly weather component
+	 */
+	visualiseWeekly = () => {
+		if (this.state.weekly == false) {
+			this.setState({weekly : true});
+		}
+		else {
+			this.setState({weekly : false});
+		}
+	}
+
+	test = () => {
+		console.log("weekly: " + this.weeklyWeather());
+	}
+
+	render() {
+		return (
+			<div class={ style.container }>
+			<div class={style.hour}>
+                    <div class={style.hourly}>
+                            <div class={style.hour}>
+                                <div class={ style.conditions }>{ this.state.tempMax1 }</div>
+                            </div>
+
+                            <div class={style.hour}>
+                                <div class={ style.conditions }>{ this.state.tempMax2 }</div>
+                            </div>
+
+                            <div class={style.hour}>
+                                <div class={ style.conditions }>{ this.state.tempMax3 }</div>
+                            </div>
+
+                            <div class={style.hour}>
+                                <div class={ style.conditions }>{ this.state.tempMax4 }</div>
+                            </div>
+
+                            <div class={style.hour}>
+                                <div class={ style.conditions }>{ this.state.tempMax5 }</div>
+                            </div>
+                        </div>
+            </div>
+			</div>
+		);
+	}
+}
